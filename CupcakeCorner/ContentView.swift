ddfd08cb74592e41cefc33b6c08cea724a3d2892
Jyothrilinga Kurapati <jyothri@jkurapati.com>
2020-04-18 17:43:03 -0700

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Jyothrilinga Kurapati on 4/18/20.
+//  Copyright © 2020 Jyothrilinga Kurapati. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
